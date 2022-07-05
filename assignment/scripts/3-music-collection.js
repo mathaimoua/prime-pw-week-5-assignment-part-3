b9aee7df092f344079e0c46a64b654fb9ed1cf31
Matt Moua <matmoua@gmail.com>
2022-07-05 15:10:06 -0500

    Added addToCollection feature

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,22 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection( title, artist, yearPublished ){
+   let album = {
+    title: title,
+    artist: artist,
+    year: yearPublished
+   }
+   //console.log( album );
+   collection.push(album);
+   return album;
+}
+
+console.log(addToCollection( "Hybrid Theory", "Linkin Park", 2000 ));
+console.log(addToCollection( "Reanimation", "Linkin Park", 2002 ));
+console.log(addToCollection( "Nurture", "Porter Robinson", 2021 ));
+console.log(addToCollection( "Take Care", "Drake", 2011 ));
+console.log(addToCollection( "Believe Acoustic", "Justin Biever", 2013 ));
+console.log(addToCollection( "Kamikaze", "Twista", 2004 ));
+
+console.log( collection );
